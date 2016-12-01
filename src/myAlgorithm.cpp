356e2a838bdaddd970c1cfa7fadb7c6cad9a807e
Idataike <julien.jakob@uha.fr>
2016-12-01 12:10:32 +0100

    Oubli de commit
    Commit

--- src/myAlgorithm.cpp
+++ src/myAlgorithm.cpp
@@ -2,12 +2,15 @@
 
 
 myAlgorithm::myAlgorithm(const Problem& pbm,const SetUpParams& setup): _setup{setup} {
+	_solution.reserve(_setup.population_size());
+	for (int i = 0; i < _setup.population_size(); ++i) {
 		Solution *s = new Solution(pbm);
 		_solution[i] = s;
 	}
 }
 
 myAlgorithm::~myAlgorithm() {
+	for (int i = 0; i < _setup.population_size(); ++i) {
 		delete _solution[i];
 	}
 }
@@ -29,6 +32,7 @@ const SetUpParams& setup() const {
 }
 
 void initialize() {
+	for (int i = 0; i < _setup.population_size(); ++i) {
 		_solution[i].initialize();
 	}
 }
